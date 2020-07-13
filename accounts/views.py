79059db52a65ad6ff88886d19a5d87ca9fc3bef3
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-07-13 16:19:44 +0100

    Sign up via django from and create  profile in the same time

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
 
 # Create your views here.
 
 
 def signUp(request):
-    pass
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/accounts/profile')
+
+    else:
+        form = SignUpForm()
+
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
