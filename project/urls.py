bc41505ae7cc5d8ba9a3ebc71251a360b17ff294
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-07-13 12:36:40 +0100

    customize account app with django registration using  MDN DOCUMENTATIONS

--- project/urls.py
+++ project/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+
     path('admin/', admin.site.urls),
-    path('jobs/', include('job.urls',namespace="jobs"))
+    path('jobs/', include('job.urls', namespace="jobs"))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
